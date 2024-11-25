7cd650bd2411e622ab4b7d2ed5284472822ace94
bileromerson <158579506+bileromerson@users.noreply.github.com>
2024-11-25 20:42:57 -0300

    foxtool Version 1.0
    the first vercion,I just to try to do something I always wanted, hope to improve it a lot still. thank you :3

--- tool.py
+++ tool.py
@@ -17,7 +17,7 @@ logo = """\033[38;5;214m
                                     
                      \033[34m[✔]       https://github.com/nome         [✔]
                      \033[34m[✔]            Version 1.0                [✔]
-                     \033[30m[-]       inspired by: hackingtool        [-]
+                     \033[30m[-]       inspired by: setoolkit        [-]
                      \033[91m[X] Please Don't Use For illegal Activity [X]
 \033[97m"""
 
